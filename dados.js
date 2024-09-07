00ddbb0e2ff459502dca7dbfbfae46019bd127a7
Thiago Alexandre <70294974+akilaoncloud@users.noreply.github.com>
2024-09-07 20:06:12 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "1",
+        descricao : "Descrição da mídia (filme, jogo ou livro)",
+        genero : "gênero da mídia (filme, jogo ou livro)",
+        link : "Link da Wikipédia sobre a mídia"
+    },
+    {
+        titulo : "2",
+        descricao : "Descrição da mídia (filme, jogo ou livro)",
+        genero : "gênero da mídia (filme, jogo ou livro)",
+        link : "Link da Wikipédia sobre a mídia"
+    },
+    {
+        titulo : "3",
+        descricao : "Descrição da mídia (filme, jogo ou livro)",
+        genero : "gênero da mídia (filme, jogo ou livro)",
+        link : "Link da Wikipédia sobre a mídia"
+    }
+];
